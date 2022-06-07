28d5689f85e55c28bd109d7caba5671d3660c3b9
Piligrim <piligrim.t@bk.ru>
2022-06-07 07:39:45 +0500

    myFirstApp https://www.internet-technologies.ru/articles/ruk-po-laravel-8-kak-sozdat-pervoe-prilozhenie.html

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    //Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+	Route::get('/dashboard',[TasksController::class, 'index'])->name('dashboard');
+
+    Route::get('/task',[TasksController::class, 'add']);
+    Route::post('/task',[TasksController::class, 'create']);
+    
+    Route::get('/task/{task}', [TasksController::class, 'edit']);
+    Route::post('/task/{task}', [TasksController::class, 'update']);
 });
